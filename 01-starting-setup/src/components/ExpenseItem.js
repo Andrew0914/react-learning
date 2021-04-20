b478a30147d5855d42f7bf2f52da45c165ee3bd1
Andrew González <andrewalangm@gmail.com>
2021-04-20 14:46:28 -0500

    🐱 Style the component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
   // only one root element ⚠️
   return (
-    <div>
+    <div className="expense-item">
       <div>March 28th 2021</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car insurance</h2>
-        <div>$284.63</div>
+        <div className="expense-item__price">$284.63</div>
       </div>
     </div>
   );
