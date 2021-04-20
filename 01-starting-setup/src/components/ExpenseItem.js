577310109220f965948ea8fa95cd5c380ff1892a
Andrew González <andrewalangm@gmail.com>
2021-04-20 14:56:05 -0500

    🐭 Show data from js {}

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 3, 20);
+  const expenseTitle = "Car insurance";
+  const expensePrice = 284.63;
   // only one root element ⚠️
   return (
+    // class is a reserved word for JS, so We need to use "className" ❕
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <div className="expense-item__price">$284.63</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
       </div>
     </div>
   );
