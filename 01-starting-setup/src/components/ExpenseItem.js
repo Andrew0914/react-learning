86ce9e99199c2d914f221d2b49770d1387ae608d
Andrew González <andrewalangm@gmail.com>
2021-04-20 14:41:17 -0500

    🐶 Add first custom react component

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  // only one root element ⚠️
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car insurance</h2>
+        <div>$284.63</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
