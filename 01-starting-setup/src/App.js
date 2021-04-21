cdfba42428e11dff040ba45f950a1ecf5c70c872
Andrew González <andrewalangm@gmail.com>
2021-04-20 20:18:20 -0500

    🍕 Move ExpenseItem's list to its own component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,6 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 function App() {
-  const expensess = [
+  const expenses = [
     { title: "Car insurance", price: 100.1, date: new Date(2021, 4, 30) },
     { title: "Toilent paper", price: 90.45, date: new Date(2021, 7, 18) },
     { title: "Candies", price: 23.4, date: new Date(2021, 6, 1) },
@@ -9,21 +9,7 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <p>This also visible!</p>
-      <ExpenseItem
-        title={expensess[0].title}
-        price={expensess[0].price}
-        date={expensess[0].date}
-      />
-      <ExpenseItem
-        title={expensess[1].title}
-        price={expensess[1].price}
-        date={expensess[1].date}
-      />
-      <ExpenseItem
-        title={expensess[2].title}
-        price={expensess[2].price}
-        date={expensess[2].date}
-      />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
---
+++ 01-starting-setup/src/components/Expenses.js
@@ -0,0 +1,25 @@
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.expenses[0].title}
+        price={props.expenses[0].price}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItem
+        title={props.expenses[1].title}
+        price={props.expenses[1].price}
+        date={props.expenses[1].date}
+      />
+      <ExpenseItem
+        title={props.expenses[2].title}
+        price={props.expenses[2].price}
+        date={props.expenses[2].date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
