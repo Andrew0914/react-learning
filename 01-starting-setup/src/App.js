7b9e66d77b0094fda415cc835b9f1c238d60fab2
Andrew González <andrewalangm@gmail.com>
2021-04-20 19:53:10 -0500

    🐕 Pass props to react component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,10 +1,29 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expensess = [
+    { title: "Car insurance", price: 100.1, date: new Date(2021, 4, 30) },
+    { title: "Toilent paper", price: 90.45, date: new Date(2021, 7, 30) },
+    { title: "Candies", price: 23.4, date: new Date(2021, 6, 30) },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This also visible!</p>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expensess[0].title}
+        price={expensess[0].price}
+        date={expensess[0].date}
+      />
+      <ExpenseItem
+        title={expensess[1].title}
+        price={expensess[1].price}
+        date={expensess[1].date}
+      />
+      <ExpenseItem
+        title={expensess[2].title}
+        price={expensess[2].price}
+        date={expensess[2].date}
+      />
     </div>
   );
 }
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 3, 20);
-  const expenseTitle = "Car insurance";
-  const expensePrice = 284.63;
+function ExpenseItem(props) {
   // only one root element ⚠️
   return (
     // class is a reserved word for JS, so We need to use "className" ❕
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expensePrice}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
       </div>
     </div>
   );
