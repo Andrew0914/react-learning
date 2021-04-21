a018284c4296ce4dc26b48a6bfa20bfff3751d95
Andrew González <andrewalangm@gmail.com>
2021-04-20 21:49:17 -0500

    🐰 Use state with reack hook useState and handle click event

--- 01-starting-setup/src/components/expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/expenses/ExpenseItem.js
@@ -1,17 +1,23 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../ui/Card";
+import { useState } from "react";
 
 function ExpenseItem(props) {
-  // only one root element ⚠️
+  const [title, setTitle] = useState(props.title);
+  // function in events must be pointend and not executed
+  const handleClick = () => {
+    setTitle("Updated!");
+  };
   return (
     // class is a reserved word for JS, so We need to use "className" ❕
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
+      <button onClick={handleClick}>Click me!</button>
     </Card>
   );
 }
