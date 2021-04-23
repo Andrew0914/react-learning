8fbfbd7e31b32f6a39052faf9ef2c853d204fd39
Andrew González <andrewalangm@gmail.com>
2021-04-23 18:29:35 -0500

    🐸 Conditional render

--- 01-starting-setup/src/components/expenses/Expenses.js
+++ 01-starting-setup/src/components/expenses/Expenses.js
@@ -5,27 +5,35 @@ import ExpensesFilter from "../expensesFilter/ExpensesFilter";
 import "./Expenses.css";
 
 function Expenses(props) {
+  // Data
   const [year, setYear] = useState("");
   const filterChangeHandler = (selectedYear) => {
     setYear(selectedYear);
   };
+  //  Logic
+  const expensesContent = () => {
+    const expenses = props.items.filter(
+      (expense) => year === "" || expense.date.getFullYear() === parseInt(year)
+    );
 
-  const filterByYear = (expense) =>
-    year === "" || expense.date.getFullYear() === parseInt(year);
+    if (expenses.length <= 0) return <p>Not expeneses found</p>;
 
+    return expenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  };
+  // Render
   return (
     <Card className="expenses">
       <div>
         <ExpensesFilter onChangeFilter={filterChangeHandler} selected={year} />
       </div>
-      {props.items.filter(filterByYear).map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent()}
     </Card>
   );
 }
