ee07a14fc2a3b1bf4032820ebab22ab79ce5795c
AndrewG <andrewalangm@gmail.com>
2021-05-01 22:50:33 -0500

    🧂 Add meals component

--- 05-practice/src/App.js
+++ 05-practice/src/App.js
@@ -1,8 +1,13 @@
 import Header from "./components/layout/Header"
+import Meals from "./components/meals/Meals"
+
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
