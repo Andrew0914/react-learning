b9803887582583292ec42491620ca11b47f5bcd2
Andrew González <andrewalangm@gmail.com>
2021-05-07 15:36:00 -0500

    🍑 Handle loading state

--- 07-http-requests/src/App.js
+++ 07-http-requests/src/App.js
@@ -5,7 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const fetchMovies = async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films");
     const moviesData = await response.json();
     const films = moviesData.results.map((movieData) => ({
@@ -15,6 +18,7 @@ function App() {
       release: movieData.release_date,
     }));
     setMovies(films);
+    setIsLoading(false);
   };
 
   return (
@@ -23,7 +27,9 @@ function App() {
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length <= 0 && <p>No movies found</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
