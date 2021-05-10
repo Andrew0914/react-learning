96b18209937eeb3d93f676edd3f726bae36df7ae
Andrew González <andrewalangm@gmail.com>
2021-05-10 13:02:07 -0500

    🍅 Handle user input

--- 09-forms/src/components/SimpleInput.js
+++ 09-forms/src/components/SimpleInput.js
@@ -1,12 +1,37 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState("");
+  const nameInputRef = useRef();
+
+  const nameChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const sutmitHandler = (event) => {
+    event.prevenDefault();
+    console.log("From state:", name);
+    console.log("From ref:", nameInputRef.current.value);
+
+    // Reset
+    setName("");
+    nameInputRef.current.value = ""; // bad practice ❌
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={sutmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={nameInputRef}
+          value={name}
+          onChange={nameChangeHandler}
+        />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="sub">Submit</button>
       </div>
     </form>
   );
