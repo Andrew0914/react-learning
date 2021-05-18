a3acbf3c715f8de6f0bd5db01b43a16092248da8
AndrewG <andrewalangm@gmail.com>
2021-05-17 20:23:55 -0500

    🍻 Re-use input hook in many inputs

--- 09-forms/src/components/BasicForm.js
+++ 09-forms/src/components/BasicForm.js
@@ -1,22 +1,79 @@
+import useInput from "../hooks/use-input";
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (email) => email.includes("@");
+
 const BasicForm = (props) => {
+  const nameInput = useInput(isNotEmpty);
+  const lastNameInput = useInput(isNotEmpty);
+  const emailInput = useInput(isEmail);
+
+  const formClass = (hasError) =>
+    hasError ? "form-control invalid" : "form-control";
+
+  const formIsVavlid =
+    !nameInput.hasError && !emailInput.hasError && !lastNameInput.hasError;
+
+  const submitHanlder = (event) => {
+    event.preventDefault();
+    if (!formIsVavlid) return;
+    console.log({
+      name: nameInput.value,
+      lastName: lastNameInput.value,
+      email: emailInput.value,
+    });
+    nameInput.reset();
+    emailInput.reset();
+    lastNameInput.reset();
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHanlder}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={`${formClass(nameInput.hasError)}`}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={nameInput.value}
+            onChange={nameInput.onChangeHandler}
+            onBlur={nameInput.onBlurHandler}
+          />
+          {!nameInput.isValid && nameInput.isTouched && (
+            <p className="error-text">Name must not be empty</p>
+          )}
         </div>
-        <div className="form-control">
-          <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+        <div className={`${formClass(nameInput.hasError)}`}>
+          <label htmlFor="lasName">Last Name</label>
+          <input
+            type="text"
+            id="lastName"
+            value={lastNameInput.value}
+            onChange={lastNameInput.onChangeHandler}
+            onBlur={lastNameInput.onBlurHandler}
+          />
+          {!lastNameInput.isValid && lastNameInput.isTouched && (
+            <p className="error-text">Las name must not be empty</p>
+          )}
         </div>
       </div>
-      <div className="form-control">
-        <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+      <div className={`${formClass(nameInput.hasError)}`}>
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+          type="text"
+          id="email"
+          value={emailInput.value}
+          onChange={emailInput.onChangeHandler}
+          onBlur={emailInput.onBlurHandler}
+        />
+        {!emailInput.isValid && emailInput.isTouched && (
+          <p className="error-text">Email must not be a valid email adress</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="submit" disabled={!formIsVavlid}>
+          Submit
+        </button>
       </div>
     </form>
   );
