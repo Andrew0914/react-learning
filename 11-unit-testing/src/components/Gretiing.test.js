caa95737069076c393710237078659af3c5e35da
AndrewG <andrewalangm@gmail.com>
2021-05-22 10:06:24 -0500

    🥮 Test user events and state

--- 11-unit-testing/src/components/Gretiing.test.js
+++ 11-unit-testing/src/components/Gretiing.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Gretting } from "./Gretting";
 describe("Gretting", () => {
   test("render gretting component", () => {
@@ -7,5 +8,37 @@ describe("Gretting", () => {
     // Act
     // Assert
     const grettingText = screen.getByText("Hello world!");
+    expect(grettingText).toBeInTheDocument()
+  });
+
+  test('Show "good to see you!" when button is not clicked', () => {
+    // Arrange
+    render(<Gretting />);
+    // Act
+    // Assert
+    const grettingText = screen.getByText("good to see you!");
+    expect(grettingText).toBeInTheDocument()
+  });
+
+  test('Show "good bye!" when button is clicked', () => {
+    // Arrange
+    render(<Gretting />);
+    const button = screen.getByRole("button")
+    // Act
+    userEvent.click(button)
+    // Assert
+    const grettingText = screen.getByText("good bye!");
+    expect(grettingText).toBeInTheDocument()
+  });
+
+  test('Not show "good to see you!" when button is clicked', () => {
+    // Arrange
+    render(<Gretting />);
+    const button = screen.getByRole("button")
+    // Act
+    userEvent.click(button)
+    // Assert
+    const grettingText = screen.queryByText("good to see you!");
+    expect(grettingText).toBeNull()
   });
 });
--- 11-unit-testing/src/components/Gretting.js
+++ 11-unit-testing/src/components/Gretting.js
@@ -1,6 +1,14 @@
+import { useState } from "react"
+
 export function Gretting(){
+  const [changedText, setChangedText] = useState(false)
+  const clickHandler = () => {
+    setChangedText(true)
+  }
   return <>
     <h2>Hello world!</h2>
-    <p>Wellcome</p>
+    {!changedText && <p>good to see you!</p>}
+    {changedText && <p>good bye!</p>}
+    <button onClick={clickHandler}>Change text</button>
   </>
 }
