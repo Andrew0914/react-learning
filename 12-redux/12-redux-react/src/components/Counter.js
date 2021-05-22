f43e658098622087b5b46edbaaf8fa0a9201b3aa
AndrewG <andrewalangm@gmail.com>
2021-05-22 18:54:18 -0500

    🍪 Dispatchs actios from components

--- 12-redux/12-redux-react/src/components/Counter.js
+++ 12-redux/12-redux-react/src/components/Counter.js
@@ -1,14 +1,20 @@
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   const toggleCounterHandler = () => {};
-
+  const incrementHandler = () => dispatch({ type: "increment" });
+  const decrementHandler = () => dispatch({ type: "decrement" });
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>+</button>
+        <button onClick={decrementHandler}>-</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
