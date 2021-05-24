9aa59e84f721185ee4d2513cbf7081d28824f4f9
AndrewG <andrewalangm@gmail.com>
2021-05-24 09:34:10 -0500

    🍲 Run side effects on components along with redux

--- 12-redux/13-redux-advanced/src/App.js
+++ 12-redux/13-redux-advanced/src/App.js
@@ -2,8 +2,18 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { FIREBASE_URL } from "./FIREBASE_SETTINGS";
 function App() {
   const cartIsVisible = useSelector((state) => state.cart.cartIsVisible);
+  const cartItems = useSelector((state) => state.cart.items);
+
+  useEffect(() => {
+    fetch(`${FIREBASE_URL}/cart.json`, {
+      method: "PUT",
+      body: JSON.stringify({ cartItems }),
+    });
+  }, [cartItems]);
   return (
     <Layout>
       {cartIsVisible && <Cart />}
