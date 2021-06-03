16f23bfdc2b90f4b744cb2a578919e61f805fef4
AndrewG <andrewalangm@gmail.com>
2021-06-03 14:55:01 -0500

    🍗 Use context along with typescript

--- 13-react-typescript/src/components/NewTodo.tsx
+++ 13-react-typescript/src/components/NewTodo.tsx
@@ -1,12 +1,11 @@
-import React, { useRef } from "react";
+import React, { useContext, useRef } from "react";
 import { Todo } from "../models/todo";
+import { TodosContext } from "../store/todos-context";
 import classes from "./NewTodo.module.css";
 
-interface INewTodoProps {
-  onAddTodo(todo: Todo): void;
-}
+const NewTodo: React.FC = (props) => {
+  const ctx = useContext(TodosContext);
 
-const NewTodo: React.FC<INewTodoProps> = (props) => {
   const todoTextInput = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -15,7 +14,7 @@ const NewTodo: React.FC<INewTodoProps> = (props) => {
     const enteredText = todoTextInput.current!.value;
     if (enteredText.trim().length === 0) return;
     const todo = new Todo(enteredText);
-    props.onAddTodo(todo);
+    ctx.addTodo(todo);
   };
   return (
     <form onSubmit={submitHandler} className={classes.form}>
--- 13-react-typescript/src/components/Todos.tsx
+++ 13-react-typescript/src/components/Todos.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-import { Todo } from "../models/todo";
+import React, { useContext } from "react";
+import { TodosContext } from "../store/todos-context";
 import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
-  props
-) => {
+const Todos: React.FC = (props) => {
+  const ctx = useContext(TodosContext);
   return (
     <ul className={classes.todos}>
-      {props.items.map((item) => (
-        <TodoItem key={item.id} item={item} onRemoveTodo={props.onRemoveTodo} />
+      {ctx.items.map((item) => (
+        <TodoItem key={item.id} item={item} onRemoveTodo={ctx.removeTodo} />
       ))}
     </ul>
   );
