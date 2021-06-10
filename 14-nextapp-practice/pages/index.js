d93a405c7c933a6daa3fe31f28be359def1c03a2
AndrewG <andrewalangm@gmail.com>
2021-06-09 19:30:17 -0500

    Fetch data for static pages

--- 14-nextapp-practice/pages/index.js
+++ 14-nextapp-practice/pages/index.js
@@ -1,30 +1,36 @@
 import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS= [
+const DUMMY_MEETUPS = [
   {
-    id:"m1",
+    id: "m1",
     title: "A first Meetup",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Jan_Vermeer_-_The_Art_of_Painting_-_Google_Art_Project.jpg/1920px-Jan_Vermeer_-_The_Art_of_Painting_-_Google_Art_Project.jpg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Jan_Vermeer_-_The_Art_of_Painting_-_Google_Art_Project.jpg/1920px-Jan_Vermeer_-_The_Art_of_Painting_-_Google_Art_Project.jpg",
     address: "Some address 10, 12345 Some city",
-    description: "This is a first meetup"
+    description: "This is a first meetup",
   },
   {
-    id:"m2",
+    id: "m2",
     title: "A second Meetup",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Sebastiano_Ricci_002.jpg/2560px-Sebastiano_Ricci_002.jpg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Sebastiano_Ricci_002.jpg/2560px-Sebastiano_Ricci_002.jpg",
     address: "Some address 69, 9657 Some city",
-    description: "This is a second meetup"
-  }
-]
-function HomePage(){
-  const [loadedMeetups, setLoadedMeetups] = useState([])
+    description: "This is a second meetup",
+  },
+];
 
-  useEffect(() => {
-    setLoadedMeetups(DUMMY_MEETUPS)
-  })
-  return <MeetupList meetups={loadedMeetups}/>
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
 
+export async function getStaticProps() {
+  // fetch data from API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
 
-export default HomePage
+export default HomePage;
