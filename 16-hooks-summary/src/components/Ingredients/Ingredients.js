1df6ea1b7db3db00334278291d6bc95ea0bf7e91
Andrew0914 <andrewalangm@gmail.com>
2021-06-28 18:34:56 -0500

    Remove ingredient

--- 16-hooks-summary/src/components/Ingredients/Ingredients.js
+++ 16-hooks-summary/src/components/Ingredients/Ingredients.js
@@ -6,19 +6,30 @@ import IngredientList from "./IngredientList";
 
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
+
   const addIngredientsHandler = (ingredient) => {
     setIngredients((prevIngridients) => [
       ...prevIngridients,
       { id: Math.random(), ...ingredient },
     ]);
   };
+
+  const removeIngridientHandler = (id) => {
+    setIngredients((prevIngridients) =>
+      prevIngridients.filter((ingridient) => ingridient.id !== id)
+    );
+  };
+
   return (
     <div className="App">
       <IngredientForm onAddIngridient={addIngredientsHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={removeIngridientHandler}
+        />
       </section>
     </div>
   );
